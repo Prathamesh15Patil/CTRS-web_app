c9477b60d0847546866ea974acf03448ebed06c8
PJPatil <prathameshjpatil15@gmail.com>
2026-05-05 18:10:28 +0530

    first commit

---
+++ Web/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-red-500 text-5xl'>Varur 2.0</div>
+    </>
+  )
+}
+
+export default App
